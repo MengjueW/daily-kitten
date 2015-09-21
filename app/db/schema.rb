31d74f8d5c602e3175e792f7b2969ba3e3bf4934
Haotian Zhang <haotian.zhang.login@gmail.com>
2015-09-20 22:19:34 -0400

    Add user name

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921014854) do
+ActiveRecord::Schema.define(version: 20150921020932) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150921014854) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
