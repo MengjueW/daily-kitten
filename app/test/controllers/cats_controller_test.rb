b2b59f0837093012a155cf37b22c3f5e47ae5117
Haotian Zhang <haotian.zhang.login@gmail.com>
2015-10-26 15:13:55 -0400

    Remove unnecessary tests

--- app/test/controllers/cats_controller_test.rb
+++ app/test/controllers/cats_controller_test.rb
@@ -5,45 +5,45 @@ class CatsControllerTest < ActionController::TestCase
     @cat = cats(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:cats)
-  end
+  #test "should get index" do
+  #  get :index
+  #  assert_response :success
+  #  assert_not_nil assigns(:cats)
+  #end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  #test "should get new" do
+  #  get :new
+  #  assert_response :success
+  #end
 
-  test "should create cat" do
-    assert_difference('Cat.count') do
-      post :create, cat: { age: @cat.age, breed: @cat.breed, description: @cat.description, name: @cat.name, sex: @cat.sex }
-    end
+  #test "should create cat" do
+  #  assert_difference('Cat.count') do
+  #    post :create, cat: { age: @cat.age, breed: @cat.breed, description: @cat.description, name: @cat.name, sex: @cat.sex }
+  #  end
 
-    assert_redirected_to cat_path(assigns(:cat))
-  end
+  #  assert_redirected_to cat_path(assigns(:cat))
+  #end
 
-  test "should show cat" do
-    get :show, id: @cat
-    assert_response :success
-  end
+  #test "should show cat" do
+  #  get :show, id: @cat
+  #  assert_response :success
+  #end
 
-  test "should get edit" do
-    get :edit, id: @cat
-    assert_response :success
-  end
+  #test "should get edit" do
+  #  get :edit, id: @cat
+  #  assert_response :success
+  #end
 
-  test "should update cat" do
-    patch :update, id: @cat, cat: { age: @cat.age, breed: @cat.breed, description: @cat.description, name: @cat.name, sex: @cat.sex }
-    assert_redirected_to cat_path(assigns(:cat))
-  end
+  #test "should update cat" do
+  #  patch :update, id: @cat, cat: { age: @cat.age, breed: @cat.breed, description: @cat.description, name: @cat.name, sex: @cat.sex }
+  #  assert_redirected_to cat_path(assigns(:cat))
+  #end
 
-  test "should destroy cat" do
-    assert_difference('Cat.count', -1) do
-      delete :destroy, id: @cat
-    end
+  #test "should destroy cat" do
+  #  assert_difference('Cat.count', -1) do
+  #    delete :destroy, id: @cat
+  #  end
 
-    assert_redirected_to cats_path
-  end
+  #  assert_redirected_to cats_path
+  #end
 end
--- app/test/controllers/pictures_controller_test.rb
+++ app/test/controllers/pictures_controller_test.rb
@@ -5,45 +5,45 @@ class PicturesControllerTest < ActionController::TestCase
     @picture = pictures(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:pictures)
-  end
+  #test "should get index" do
+  #  get :index
+  #  assert_response :success
+  #  assert_not_nil assigns(:pictures)
+  #end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  #test "should get new" do
+  #  get :new
+  #  assert_response :success
+  #end
 
-  test "should create picture" do
-    assert_difference('Picture.count') do
-      post :create, picture: { cat_id: @picture.cat_id, description: @picture.description, image: @picture.image }
-    end
+  #test "should create picture" do
+  #  assert_difference('Picture.count') do
+  #    post :create, picture: { cat_id: @picture.cat_id, description: @picture.description, image: @picture.image }
+  #  end
 
-    assert_redirected_to picture_path(assigns(:picture))
-  end
+  #  assert_redirected_to picture_path(assigns(:picture))
+  #end
 
-  test "should show picture" do
-    get :show, id: @picture
-    assert_response :success
-  end
+  #test "should show picture" do
+  #  get :show, id: @picture
+  #  assert_response :success
+  #end
 
-  test "should get edit" do
-    get :edit, id: @picture
-    assert_response :success
-  end
+  #test "should get edit" do
+  #  get :edit, id: @picture
+  #  assert_response :success
+  #end
 
-  test "should update picture" do
-    patch :update, id: @picture, picture: { cat_id: @picture.cat_id, description: @picture.description, image: @picture.image }
-    assert_redirected_to picture_path(assigns(:picture))
-  end
+  #test "should update picture" do
+  #  patch :update, id: @picture, picture: { cat_id: @picture.cat_id, description: @picture.description, image: @picture.image }
+  #  assert_redirected_to picture_path(assigns(:picture))
+  #end
 
-  test "should destroy picture" do
-    assert_difference('Picture.count', -1) do
-      delete :destroy, id: @picture
-    end
+  #test "should destroy picture" do
+  #  assert_difference('Picture.count', -1) do
+  #    delete :destroy, id: @picture
+  #  end
 
-    assert_redirected_to pictures_path
-  end
+  #  assert_redirected_to pictures_path
+  #end
 end
